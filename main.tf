090ab515b2e07a200fe526c1f3c6da1ca8b6d065
mana s <curry123456789@gmail.com>
2022-11-08 05:25:49 +0900

    aws_s3_bucket b to main

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "main" {
   bucket = "my-tf-test-bucket-000000001"
 
   tags = {
@@ -8,12 +8,12 @@ resource "aws_s3_bucket" "b" {
 }
 
 resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
+  bucket = aws_s3_bucket.main.id
   acl    = "private"
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.b.id
+  bucket = aws_s3_bucket.main.id
 
   block_public_acls       = true
   block_public_policy     = true
