fa0bc27e89a51fbbef10366f6e2db6420e2f1819
mana s <curry123456789@gmail.com>
2022-11-07 22:41:34 +0900

    terraform fmt

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   cloud {
     organization = "curry9999"
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
 
     workspaces {
       name = "mac-to-s3-backup"
