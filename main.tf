0bfb8aeae9d7ec860a83af603d11e8da531e1acb
mana s <curry123456789@gmail.com>
2022-11-09 23:28:17 +0900

    add aws_s3_bucket_versioning

--- main.tf
+++ main.tf
@@ -7,16 +7,23 @@ resource "aws_s3_bucket" "main" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "main" {
   bucket = aws_s3_bucket.main.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
+resource "aws_s3_bucket_public_access_block" "main" {
   bucket = aws_s3_bucket.main.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
