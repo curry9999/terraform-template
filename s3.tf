7ba2ec6a01a3425c41d3c00e5e7aaf18051ce8dc
mana s <curry123456789@gmail.com>
2022-11-11 02:19:29 +0900

    add variable

--- s3.tf
+++ s3.tf
@@ -2,8 +2,8 @@ resource "aws_s3_bucket" "main" {
   bucket = var.s3_bucket_name
 
   tags = {
-    Name        = "Mac to S3 Backup Bucket"
-    Environment = "Prod"
+    Name        = var.s3_bucket_name
+    Environment = var.s3_bucket_env
   }
 }
 
