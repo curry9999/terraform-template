177b3232c44c9319fa596845c44fa28be68dbb4d
mana s <curry123456789@gmail.com>
2022-11-13 08:33:22 +0900

    terraform fmt

--- environments/s3/prod/main.tf
+++ environments/s3/prod/main.tf
@@ -1,6 +1,6 @@
 module "prod" {
-  source = "../../../modules/s3"
-  s3_bucket_name = "mac-backup-bucket-000000001"
-  s3_bucket_env = "Prod"
+  source           = "../../../modules/s3"
+  s3_bucket_name   = "mac-backup-bucket-000000001"
+  s3_bucket_env    = "Prod"
   aws_profile_name = "terraform-prod"
 }
