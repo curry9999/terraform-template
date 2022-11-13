b7e7947bdfb5fe2bba73ad9955265cf48000a0ba
mana s <curry123456789@gmail.com>
2022-11-13 23:07:09 +0900

    add s3 bucket log

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -1,3 +1,28 @@
+# S3 Log Bucket
+resource "aws_s3_bucket" "log" {
+  bucket = join("-",[var.s3_bucket_name,"logs"])
+
+  tags = {
+    Name        = var.s3_bucket_name
+    Environment = var.s3_bucket_env
+  }
+}
+
+resource "aws_s3_bucket_acl" "log" {
+  bucket = aws_s3_bucket.log.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "log" {
+  bucket = aws_s3_bucket.log.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# S3 Bucket
 resource "aws_s3_bucket" "main" {
   bucket = var.s3_bucket_name
 
@@ -26,4 +51,11 @@ resource "aws_s3_bucket_versioning" "main" {
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+resource "aws_s3_bucket_logging" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  target_bucket = aws_s3_bucket.log.id
+  target_prefix = "log/"
 }
